2e2d12916bb12ab74783c91bc3617c8fe41ffedf
Natig <natikibishov@gmail.com>
2021-09-26 01:03:19 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,66 +1,167 @@
-import React, { useState,useEffect } from "react"
+import React, { useState } from 'react'
 import Highcharts from 'highcharts/highstock'
 import HighchartsReact from 'highcharts-react-official'
 import HC_exporting from 'highcharts/modules/exporting'
 import './App.css'
 import DropDown from './Components/DrowDown/DropDown'
-import {highchartsCallback} from "./Components/Buttons/Buttons";
 import data from './data.json'
-import {configObj} from './Components/Utils/ConfigObj'
+
+
+function getData() {
+    for (const item of data) {
+        item.visible = true
+    }
+    return data
+}
 
 const App = () => {
 
     HC_exporting(Highcharts)
 
-    const items = [
+    const [rows, setRows] = useState(() => getData())
+
+    const [chartOptions] = useState({
+        chart: {
+            type: 'scatter',
+            margin: [70, 50, 60, 80],
+            backgroundColor: 'whitesmoke',
+            accessibility: {
+                enabled: true
+            },
+            events: {
+                click: function (e) {
+
+                    const x = Math.round(e.xAxis[0].value),
+                        y = Math.round(e.yAxis[0].value),
+                        series = this.series[0]
+
+                    series.addPoint([x, y])
+                }
+            },
+
+        },
+        title: {
+            text: ''
+        },
+        subtitle: {
+            text: ''
+        },
+        xAxis: {
+            gridLineWidth: 1,
+            maxPadding: 0.2,
+        },
+        yAxis: {},
+        legend: {
+            enabled: false,
+            layout: 'horizontal',
+            align: 'right',
+            verticalAlign: 'bottom',
+            className: 'charts',
+            itemWidth: 140,
+            y: 0,
+
+        },
+        plotOptions: {
+            series: {
+                lineWidth: 1,
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 1,
+                point: {
+                    events: {
+                        click: function () {
+                            if (this.series.data.length > 1) {
+                                this.remove()
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        series: rows,
+        credits: {
+            enabled: false
+        },
+    })
+
+    const headings = [
         {value:'Показатель'},
         {value:'Текущий день'},
         {value:'Вчера'},
         {value:'Этот день недели'}
         ]
+
+    const updateVisible = (visible, index) => {
+        setRows((oldRows) => {
+            oldRows[index].visible = visible
+            return JSON.parse(JSON.stringify(oldRows))
+        })
+    }
+
   return (
       <div className='App'>
-          <DropDown data={data}
-                    callback={highchartsCallback}
-          />
-              <table className='table'>
-                       <tr>
-                          {items.map((item,index) =>
-                              <th>{item.value}</th>
-                          )}
-                      </tr>
-                  {data.map((item,index) =>
-                      <>
+          <table className='table'>
+              <thead>
+              <tr>
+                  <th className='main-header' colSpan='4'>
+                      Общая Статистика
+                      <DropDown rows={rows} updateVisible={updateVisible}/>
+                  </th>
+              </tr>
+              <tr>
+                  {headings.map((heading) =>
+                      <th>{heading.value}</th>
+                  )}
+              </tr>
+              </thead>
+              <tbody>
+              {rows.map((row, index) => {
+                      return <>
                           {index === 1 &&
                           <tr>
                               <td colSpan='4'>
                                   <HighchartsReact
                                       className='charts'
                                       highcharts={Highcharts}
-                                      options={configObj}
+                                      options={chartOptions}
                                       constructorType={'chart'}
-                                      callback={highchartsCallback}
                                   />
                               </td>
                           </tr>
                           }
-                          <tr>
-                              <td>{item.name}</td>
+                          {row.visible &&
+                          <tr key={index}>
+                              <td>{row.name}</td>
                               {
-                                  <>
-                              {item.data.map((item,index) =>
-                                      <td
-                                          id={item.id}
-                                          key={item.id}>{item}</td>
-                                  )}
-                                  </>
+                                  row.data.map((num, index) => {
+                                      if (index === 1) {
+                                          const difference = Math.round((1 - row.data[1] / row.data[0]) * 100)
+                                          return <td
+                                              className={difference <= -10 ? 'red' : difference >= 10 ? 'green' : ''}
+                                              key={index}
+                                          >
+                                              <div className='number'>{num}</div>
+                                              <div className='difference'>{difference}%</div>
+                                          </td>
+                                      } else {
+                                          return <td key={index}>
+                                              <div className='number'>{num}</div>
+                                          </td>
+                                      }
+
+                                  })
                               }
                           </tr>
+                          }
+
                       </>
-                  )}
-              </table>
-          </div>
-  );
-};
+                  }
+              )}
+              </tbody>
+          </table>
+      </div>
+  )
+}
 
 export default App
--- src/Components/DrowDown/DropDown.js
+++ src/Components/DrowDown/DropDown.js
@@ -1,47 +1,32 @@
-import React, {useState} from "react"
+import React, { useState } from 'react'
 import './DrowDown.css'
 import SettingsIcon from '@material-ui/icons/Settings'
+import Checkbox from './Checkbox'
 
-const DropDown = ({ data }) => {
-
+const DropDown = ({ rows, updateVisible }) => {
     const [settingsMenuOpen, setSettingsMenuOpen] = useState(false)
 
-    /*const handleChange = (e) => {
-        let x = e.target.value
-    }*/
-
     return (
-        <div>
+        <div className='settings'>
             <SettingsIcon className='button' onClick={() => setSettingsMenuOpen(!settingsMenuOpen)}/>
-            <div className={!settingsMenuOpen ? 'settings__menu active' : 'settings__menu'}
+            <div className={`settings__menu ${settingsMenuOpen ? 'active' : ''}`}
                  onClick={() => setSettingsMenuOpen(false)}>
+
                 <div onClick={e => e.stopPropagation()}>
+
                     <ul>
-                        <li>
-                            <label>
-                                {/*{data.map((item, index) =>
-                                    <li>
-                                        <label>
-                                            { <input type="checkbox"
-                                                     value={index}
-                                                     onChange={handleChange}
-                                                     id={item.id} key={item.id} /> }
-                                            {item.name}
-                                        </label>
-                                    </li>)
-                                }*/}
-                                <table className='table'>
-                                    <tbody>
-                                    </tbody>
-                                </table>
-                            </label>
-                        </li>
+                        {rows.map((row, index) =>
+                            <li key={index}>
+                                <label>
+                                    <Checkbox row={row} key={index} index={index} updateVisible={updateVisible} /> {row.name}
+                                </label>
+                            </li>)
+                        }
                     </ul>
                 </div>
             </div>
         </div>
-    );
-};
-
+    )
+}
 
 export default DropDown
